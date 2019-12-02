a152c1f123d413add9a947c9b53b62e90b6035e1
samuelowino <samuelowino43@yahoo.com>
2019-12-02 12:59:18 +0300

    Payment confirmation UI refactor

--- app/src/main/java/org/aplusscreators/hakikisha/views/common/PaymentConfirmationActivity.java
+++ app/src/main/java/org/aplusscreators/hakikisha/views/common/PaymentConfirmationActivity.java
@@ -1,6 +1,8 @@
 package org.aplusscreators.hakikisha.views.common;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.aplusscreators.hakikisha.R;
 
 public class PaymentConfirmationActivity extends AppCompatActivity {
+
+    private View closeView;
+    private TextView transactionStatusTextView;
+    private TextView transactionIdTextView;
+    private TextView amountTextView;
+    private TextView vendorNameTextView;
+    private TextView vendorEmailTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
