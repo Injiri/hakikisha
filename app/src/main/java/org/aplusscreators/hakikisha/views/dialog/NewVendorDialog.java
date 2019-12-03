239232e07cbe2b1798069c2e999e9bae4e0f8d57
samuelowino <samuelowino43@yahoo.com>
2019-12-04 01:35:59 +0300

    New vendors dialog actions

--- app/src/main/java/org/aplusscreators/hakikisha/views/dialog/NewVendorDialog.java
+++ app/src/main/java/org/aplusscreators/hakikisha/views/dialog/NewVendorDialog.java
@@ -2,21 +2,31 @@ package org.aplusscreators.hakikisha.views.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.aplusscreators.hakikisha.R;
+import org.aplusscreators.hakikisha.views.common.VendorFormActivity;
 
 public class NewVendorDialog extends Dialog {
 
     private FloatingActionButton addVendorFab;
-    private FloatingActionButton submitNewVendorFab;
+    private FloatingActionButton submitVendorFab;
     private Spinner vendorsSpinner;
-
+    private ArrayAdapter<String> vendorsAdapter;
+    private String[] vendors = {
+            "Olx","Jumia","Alibaba"
+    };
+    
     public NewVendorDialog(@NonNull Context context) {
         super(context);
     }
@@ -25,5 +35,27 @@ public class NewVendorDialog extends Dialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_vendor_dialog);
+
+        this.addVendorFab = findViewById(R.id.activity_new_vendor_fab);
+        this.submitVendorFab = findViewById(R.id.submit_vendor_button);
+        this.vendorsSpinner = findViewById(R.id.vendor_options_spinner);
+        this.vendorsAdapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_spinner_dropdown_item,vendors);
+        this.vendorsSpinner.setAdapter(vendorsAdapter);
+
+        this.addVendorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), VendorFormActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
+
+        this.submitVendorFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+                Toast.makeText(getContext(),"Vendor added successfully",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
