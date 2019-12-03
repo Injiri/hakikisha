5df8a99ab348533d2b4023373e3842af4dc82e88
samuelowino <samuelowino43@yahoo.com>
2019-12-03 11:51:56 +0300

    Payment request UI implementation

---
+++ app/src/main/java/org/aplusscreators/hakikisha/views/common/PaymentRequestActivity.java
@@ -0,0 +1,17 @@
+package org.aplusscreators.hakikisha.views.common;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.aplusscreators.hakikisha.R;
+
+public class PaymentRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_request);
+    }
+}
