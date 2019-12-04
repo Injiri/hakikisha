29e3bf881628a64cf3df0166ba1a340056f53f10
samuelowino <samuelowino43@yahoo.com>
2019-12-04 15:31:22 +0300

    Customer profile image setup actions

--- app/src/main/java/org/aplusscreators/hakikisha/views/common/CustomerFormActivity.java
+++ app/src/main/java/org/aplusscreators/hakikisha/views/common/CustomerFormActivity.java
@@ -1,28 +1,48 @@
 package org.aplusscreators.hakikisha.views.common;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.squareup.picasso.Picasso;
 
 import org.aplusscreators.hakikisha.R;
+import org.aplusscreators.hakikisha.settings.HakikishaPreference;
+import org.aplusscreators.hakikisha.utils.FileUtils;
+
+import java.io.File;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CustomerFormActivity extends AppCompatActivity {
 
+    private static final String TAG = "CustomerFormActivity";
+    private static final int CAMERA_CAPTURE_REQUEST_CODE = 5551;
+    private static final int CAMERA_PERMISSIONS_REQUEST_CODE = 6661;
     CircleImageView customerProfileImageView;
     View closeFormView;
     EditText customerNamesEditText;
     EditText customerPhoneEditText;
     EditText customerEmailEditText;
     FloatingActionButton addCustomerFab;
+    private File imageFile;
+    private Uri photoUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,5 +78,45 @@ public class CustomerFormActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        this.customerProfileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptCameraImageCapture();
+            }
+        });
+    }
+
+    private void attemptCameraImageCapture() {
+        if (ActivityCompat.checkSelfPermission(CustomerFormActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            Intent camerCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            imageFile = FileUtils.createImageFile(getApplicationContext(), "hks_customer_"+ UUID.randomUUID().toString(), ",png");
+            photoUri = FileProvider.getUriForFile(getApplicationContext(), "org.aplusscreators.hakikisha.fileProvider", imageFile);
+            camerCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            if (camerCaptureIntent.resolveActivity(getPackageManager()) == null) {
+                Toast.makeText(getApplicationContext(), "Unable to open camera, try again later", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            startActivityForResult(camerCaptureIntent, CAMERA_CAPTURE_REQUEST_CODE);
+        } else {
+            ActivityCompat.requestPermissions(CustomerFormActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAMERA_CAPTURE_REQUEST_CODE && resultCode == Activity.RESULT_OK && photoUri != null) {
+            try {
+                Picasso.get().load(photoUri).into(customerProfileImageView);
+                ObjectMapper objectMapper = new ObjectMapper();
+                String imageFileSerialized = objectMapper.writeValueAsString(imageFile);
+                HakikishaPreference.setUserProfileUriPref(CustomerFormActivity.this, imageFileSerialized);
+            } catch (Exception ex) {
+                Log.e(TAG, "onActivityResult: " + ex);
+            }
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
