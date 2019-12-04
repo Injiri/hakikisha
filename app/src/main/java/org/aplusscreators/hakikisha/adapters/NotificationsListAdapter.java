1b7033a0a2205ede140ec79d4c692dc02a5e5f48
samuelowino <samuelowino43@yahoo.com>
2019-12-04 15:42:58 +0300

    Notifications adapter

---
+++ app/src/main/java/org/aplusscreators/hakikisha/adapters/NotificationsListAdapter.java
@@ -0,0 +1,21 @@
+package org.aplusscreators.hakikisha.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotificationsListAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
