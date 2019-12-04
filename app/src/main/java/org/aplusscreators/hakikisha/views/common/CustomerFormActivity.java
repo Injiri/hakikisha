d8eb352fd7f5126b5265acc1dde42bcdb0a82b76
samuelowino <samuelowino43@yahoo.com>
2019-12-04 14:55:15 +0300

    Add customer form design

---
+++ app/src/main/java/org/aplusscreators/hakikisha/views/common/CustomerFormActivity.java
@@ -0,0 +1,17 @@
+package org.aplusscreators.hakikisha.views.common;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.aplusscreators.hakikisha.R;
+
+public class CustomerFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_customer);
+    }
+}
